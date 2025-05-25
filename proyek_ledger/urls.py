38553ed9e27afaacc1756a617266cdc55ad02bf3
fredleluly <fredleluly@gmail.com>
2025-05-25 20:07:15 +0700

    endpoint

--- proyek_ledger/urls.py
+++ proyek_ledger/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('ledger/', include('django_ledger.urls', namespace='django_ledger')),
 ]
